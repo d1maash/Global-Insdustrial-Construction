b94f41844d3ae85141907349159f583828f6ff68
Dimash <dimashaijarikov@gmail.com>
2024-11-13 15:37:02 +0500

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -314,5 +314,13 @@ function startCarousel() {
 
 window.addEventListener('load', startCarousel);
 
-
+$('.carousel .carousel-item').each(function () {
+    var minPerSlide = 4;
+    var next = $(this).next();
+    if (!next.length) {
+    next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+    
+    for (var i = 0; i < minPerSlide; i++) { next=next.next(); if (!next.length) { next=$(this).siblings(':first'); } next.children(':first-child').clone().appendTo($(this)); } });
 
